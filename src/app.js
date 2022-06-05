06a041da309bb12573d56415ab5104132c9fe6d4
paurnima-janrao <paurnimajanrao875@gmail.com>
2022-06-05 15:03:03 +0530

    Added Server with 4 endpoints for crud

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 let articleSelected = false;
 let participants = $("#participants");
 let selectedArticle = {}
+let dummyParticipants = [];
 
 ///Perform get articles operation here
-
-dummyParticipants.forEach(item => {
-    participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle("+item.id+")\">Delete</button></div>");
+$.get("http://localhost:9000/api/v1/getArticles", function (data, status) {
+    dummyParticipants = [...data];
+    dummyParticipants.forEach(item => {
+        participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle(" + item.id + ")\">Delete</button></div>");
+    });
 });
+
+
+
 if (!articleSelected) {
     articleSelected = true;
     $(".articleBoard").hide();
@@ -24,19 +30,20 @@ function selectArticle(articalNumber) {
             selectedArticle.id = articalNumber;
             selectedArticle.title = el.title;
             selectedArticle.html = el.html;
+            selectedArticle.name = el.name;
         }
     })
 }
 
-function createArticle(){
+function createArticle() {
     $(".articleBoard").show();
     $('#previewArticle').hide();
     $('#newArticle').show();
     $('.hiddenActionsNew').show();
 }
 
-function createNewArticle(){
-    selectedArticle.id = dummyParticipants[dummyParticipants.length-1].id+1;
+function createNewArticle() {
+    selectedArticle.id = dummyParticipants[dummyParticipants.length - 1].id + 1;
     selectedArticle.name = $('#name').val();
     selectedArticle.title = $('#newTitle').val();
     selectedArticle.html = $('#newArticleBody').val();
@@ -44,27 +51,51 @@ function createNewArticle(){
     participants.empty();
     participants.append("<div class=\"participantsHeader\">ARTICLES</div>");
     dummyParticipants.forEach(item => {
-        participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle("+item.id+")\">Delete</button></div>");
+        participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle(" + item.id + ")\">Delete</button></div>");
     });
 
 
     ///Perform create operation here
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:9000/api/v1/addArticle",
+        data: selectedArticle,
+        success: function (data) {
+            console.log(data);
+        }
+    });
 
-    location.reload();
-    
 
 }
 
-function deleteArticle(articalNumber){
-    let index = dummyParticipants.findIndex(el=>el.id==articalNumber);
-    if(index!=-1){
-        dummyParticipants.splice(index,1)
+function cancelNewArticle() {
+    $(".articleBoard").hide();
+    $('#previewArticle').show();
+    $('#newArticle').hide();
+    $('.hiddenActionsNew').hide();
+}
+
+function deleteArticle(articalNumber) {
+    let index = dummyParticipants.findIndex(el => el.id == articalNumber);
+    if (index != -1) {
+        dummyParticipants.splice(index, 1)
     }
     participants.empty();
     participants.append("<div class=\"participantsHeader\">ARTICLES</div>");
     dummyParticipants.forEach(item => {
-        participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle("+item.id+")\">Delete</button></div>");
+        participants.append("<div class=\"participant\" id=" + item.id + ">" + item.name + "<button style=\"float:right; cursor:pointer;\" onclick=\"deleteArticle(" + item.id + ")\">Delete</button></div>");
     });
+    $.ajax({
+        type: "post",
+        url: "http://localhost:9000/api/v1/deleteArticle",
+        data: {
+            "id": articalNumber
+        },
+        success: function (data) {
+            console.log(data);
+        }
+    });
+    location.reload();
 }
 
 function saveChanges() {
@@ -76,6 +107,15 @@ function saveChanges() {
     selectedArticle.html = newText;
 
     ///Perform update call here
+    $.ajax({
+        type: "post",
+        url: "http://localhost:9000/api/v1/editArticle",
+        data: selectedArticle,
+        success: function (data) {
+            console.log(data);
+        }
+    });
+    location.reload();
 }
 
 function cancelChanges() {
